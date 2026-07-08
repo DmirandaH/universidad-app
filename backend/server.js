a61ad80d1de2ac57db2667ae6ccdbd4489015948
mirdayan <dmirandaherrera@gmail.com>
2026-07-07 21:30:52 -0500

    creando un objecto de nombre estudiantes.

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,33 @@ const app = express();
 //Definimos el puerto
 const PORT = 3000;
 
+
+
 //Ruta principal
 app.get('/', (req, res) => {
     res.send('Servidor funcionando correctamente');
 
 });
 
+app.get('/estudiantes', (req, res) => {
+
+    //creo el objecto estudiante 
+
+    const estudiante = {
+        id: 1,
+        nombre: 'dayan',
+        curso: 'creacion Api',
+        correo: 'mirdev@corp.com' 
+    }
+
+// // Simulamos un estudiante obtenido desde la base de datos
+    res.json(estudiante);
+
+});
+
+
+
 // Iniciamos el servidor
 app.listen(PORT, () => {
-    console.log('servifor escuchnado en http://localhost:${PORT}');
+    console.log(`servidor escuchando en http://localhost:${PORT}`);
 });
